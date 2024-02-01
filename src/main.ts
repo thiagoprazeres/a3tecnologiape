b0add1814e6acc9ae12bd7127028254b9b52d383
Thiago Prazeres <thiago@192.168.1.13>
2024-01-31 22:59:53 -0300

    swiper

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
+import 'zone.js';
+import { register as registerSwiperElements } from 'swiper/element/bundle';
 import { AppComponent } from './app/app.component';
+
+registerSwiperElements();
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
