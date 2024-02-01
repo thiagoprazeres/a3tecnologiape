0b50d1208c4b1d93933202f27f84c2274c4152cb
Thiago Prazeres <thiago@192.168.1.13>
2024-02-01 01:33:48 -0300

    conteúdo e menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
+  open = false;
 }
